136c8f2f0edcdb1879f0c6ef58607baf7ce00aab
Habib16051 <habibmhr143.com>
2023-10-02 11:28:39 +0600

    blog list and blog detail updated and image added

--- agro/models.py
+++ agro/models.py
@@ -47,7 +47,11 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     date_posted = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-date_posted', ]
 
     def __str__(self):
         return self.title
